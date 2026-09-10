de8448abecb1e350aa1f351370e2c30b0305f731
Saraansh <gandhisaraansh@gmail.com>
2026-09-10 22:07:11 +0530

    hashset satred

---
+++ Phase 1 - Basic HashMap/basicFromCourse.java
@@ -0,0 +1,11 @@
+class basicFromCourse {
+
+  public static void main ( String args[] ){
+
+    // in hashset -> insert , delete , search has tc of o(1) and total space is o(n) where n is the number of elements in the hashset
+
+
+
+  }
+
+}
