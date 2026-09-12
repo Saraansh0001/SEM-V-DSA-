7b800ca043a088588a62f29994b3cbf11d6681d2
Saraansh <gandhisaraansh@gmail.com>
2026-09-12 12:44:19 +0530

    hashmap ; pairs with diff k done

---
+++ Phase 1 - Basic HashMap/GFG/pairsWithDiffK.java
@@ -0,0 +1,22 @@
+class Solution {
+    int countPairs(int[] arr, int k) {
+
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+
+            if (map.containsKey(num - k)) {
+                count += map.get(num - k);
+            }
+
+            if (map.containsKey(num + k)) {
+                count += map.get(num + k);
+            }
+
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+    }
+}
