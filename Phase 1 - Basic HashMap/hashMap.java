772478bc609a2ea27e1e45c038fcf794b61a7958
Saraansh <gandhisaraansh@gmail.com>
2026-09-12 15:23:45 +0530

    hashmap ; hashset

---
+++ Phase 1 - Basic HashMap/hashMap.java
@@ -0,0 +1,12 @@
+import java.util.* ;
+
+class basicFromCourse {
+
+  public static void main ( String args[] ){
+
+    HashMap<Integer,Integer> map = new HashMap<>(); // key value pair
+
+  }
+}
+
+// i got my eyes on you
